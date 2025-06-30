f68d00a1e16a4245edc935511707a92e682562f6
MohdShumail1122 <108707795+MohdShumail1122@users.noreply.github.com>
2025-06-30 13:58:40 +0000

    data types

---
+++ basics/dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello")   in node js we cant use like this as we use in browser
+
+// Also Refer MDN Documentation
+
+let name = "shumail"
+let age = 24
+let isLoggedIn = false
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 12);
+console.log(typeof "SHUAMIL");
+console.log(typeof "");
+
+
+
+
+
+// PRIMITIVE DATA TYPES
+
+//  number -> 2 to the power 53
+//  bigInt
+//  String -> ""
+//  boolean -> true/false
+//  null -> standAlone Value 
+//  undefined
+//  symbol -> used for uniqueness
+
+
+
+// OTHER DATA TYPES
+
+// object 
