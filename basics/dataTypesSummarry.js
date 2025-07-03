76fe698a0dc130b3a0ba58e57b096bc6c9b83e92
MohdShumail1122 <108707795+MohdShumail1122@users.noreply.github.com>
2025-07-03 12:21:39 +0000

    data type summary

---
+++ basics/dataTypesSummarry.js
@@ -0,0 +1,59 @@
+//  Primitive DATA Type
+
+/*
+They are called by Value rather than refernce
+
+String
+Number
+Boolean
+Null
+Undefined. we have declared it but not assign the value
+symbol
+BigInt. for Bigger values
+*/
+
+
+// *************. Non Primitive *************
+
+
+/*
+They are Call By Refrence
+array , Objects , Function
+*/
+
+// JS is a Dynamically Typed Language
+// unlike Ktlin or typeScript we dont define the type preamptly
+
+
+// symbol
+
+// const id1 = Symbol('123')
+// const id2 = Symbol('123')
+
+// console.log(id1);
+// console.log(id2);
+// console.log(id1 == id2);
+// console.log(id1 === id2);  
+
+// Even though the value looking is same but in actual its not same
+
+
+// *************. Non Primitive ****************
+
+const heros = ["ironmen", "supermen", "batmen"]
+console.log(heros);
+console.log(typeof(heros));
+
+let myObject = {
+    name: "Shumail",
+    age: 23
+}
+
+let myFun = function(){
+    console.log("hello world");
+    
+}
+console.log(myFun);
+
+
+
