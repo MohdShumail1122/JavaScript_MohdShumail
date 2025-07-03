3d95d8ddc5ea90073077120a3bb6702cd48eb99a
MohdShumail1122 <108707795+MohdShumail1122@users.noreply.github.com>
2025-07-03 11:29:48 +0000

    data type conversion

---
+++ basics/dataTypesConversions.js
@@ -0,0 +1,43 @@
+// Conversion and Operation in JavaScript
+
+// let score = "33"
+// let score1 = "222abs"
+// let age = 24
+// let date = null
+
+// console.log(typeof(score));
+// console.log(typeof(score1));
+// console.log(typeof(age));
+// console.log(typeof(date));
+
+
+// let score1InNumber = Number(score)
+// console.log(score1InNumber);
+// console.log(typeof(score1InNumber));
+
+// let valueInNumber = Number(score1)
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+
+// let dateInNumber = Number(date)
+// console.log(dateInNumber);
+// console.log(typeof(dateInNumber));
+
+
+
+
+let isLoggedIn = 0 // Zero and empty strin will convert to false 
+// rather 1 and not empty String will convert to true
+
+let booleanLoogedIn = Boolean(isLoggedIn)
+console.log(booleanLoogedIn);
+console.log(typeof(booleanLoogedIn));
+
+let numberInString = String(isLoggedIn)
+console.log(numberInString);
+console.log(typeof(numberInString));
+
+
+// Hence we can convert any data type to any thing Just We carefull with String to Number 
+
